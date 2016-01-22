263d5f59e68db007007037a6a65d95307df02d92
akiraaida <akira.aida@live.com>
2016-01-22 14:36:07 -0500

    Removing duplicate files

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf "\nPreparing to identify the differencecs between files.\n"
-printf "1. $1\n"
-printf "2. $2\n"
-printf "The differences between the two files are...\n"
-diff "$1" "$2"
