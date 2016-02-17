f45fc1bd061d9c20c977b2af76c97e7e634b7976
Kathryn McKay <kathryn.mckay@uoit.net>
2016-02-17 10:18:56 -0500

    added header at begining of main

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,4 +1,25 @@
-
+/******************************************************************************
+* main.cpp
+* CSCI 3060u/SOFE 3980u: Course Project Front End
+* Winter 2016
+*
+* Shotokan Tigers:
+* -----
+* Akira Aida          100526064
+* Kathryn McKay       100524201
+* Alexander Wheadon   100514985
+*
+* Program Intention:
+* ----
+*
+* Input & Output Files:
+* ----
+*
+*
+* How the Program Is Intended to Be Run:
+* ----
+*
+*******************************************************************************/
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
