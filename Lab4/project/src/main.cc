eebb700f1d36c469d5a17ff11cda6de1addf74f7
Kathryn McKay <kathryn.mckay@uoit.net>
2016-02-26 15:38:27 -0500

    now stops execution when cin closes

--- Lab4/project/src/main.cc
+++ Lab4/project/src/main.cc
@@ -57,30 +57,30 @@ int main(int argc, const char* argv[]) {
     }
 
     char user_cmd[11] = { 0 };
-    while (true) {
+    while (std::cin.good()) {
       std::cout << PROMPT_ENTER_COMMAND << std::endl;
       std::cin.getline(user_cmd, sizeof(user_cmd));
-      if (strncmp(user_cmd, "login", 10) == 0) {                    
+      if (strncmp(user_cmd, "login", 10) == 0) {
         commands.login();
-      } else if (strncmp(user_cmd, "withdrawal", 10) == 0) {         
+      } else if (strncmp(user_cmd, "withdrawal", 10) == 0) {
         commands.withdrawal();
-      } else if (strncmp(user_cmd, "transfer", 10) == 0) {           
+      } else if (strncmp(user_cmd, "transfer", 10) == 0) {
         commands.transfer();
-      } else if (strncmp(user_cmd, "paybill", 10) == 0) {           
+      } else if (strncmp(user_cmd, "paybill", 10) == 0) {
         commands.paybill();
-      } else if (strncmp(user_cmd, "deposit", 10) == 0) {           
+      } else if (strncmp(user_cmd, "deposit", 10) == 0) {
         commands.deposit();
-      } else if (strncmp(user_cmd, "create", 10) == 0) {            
+      } else if (strncmp(user_cmd, "create", 10) == 0) {
         commands.create();
-      } else if (strncmp(user_cmd, "delete", 10) == 0) {            
+      } else if (strncmp(user_cmd, "delete", 10) == 0) {
         commands.delete_account();
-      } else if (strncmp(user_cmd, "disable", 10) == 0) {            
+      } else if (strncmp(user_cmd, "disable", 10) == 0) {
         commands.disable();
-      } else if (strncmp(user_cmd, "enable", 10) == 0) {            
+      } else if (strncmp(user_cmd, "enable", 10) == 0) {
         commands.enable();
-      } else if (strncmp(user_cmd, "changeplan", 10) == 0) {        
+      } else if (strncmp(user_cmd, "changeplan", 10) == 0) {
         commands.changeplan();
-      } else if (strncmp(user_cmd, "logout", 10) == 0) {           
+      } else if (strncmp(user_cmd, "logout", 10) == 0) {
         commands.logout();
       } else {
         std::cout << ERROR_MESSAGE_INVALID_COMMAND << std::endl;
