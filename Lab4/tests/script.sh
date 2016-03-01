9e8afd1a1dd0b638d8d5d4aab9114f70a9b20998
Kathryn McKay <kathryn.mckay@uoit.net>
2016-03-01 13:25:20 -0500

    began implementing use of transactions

--- Lab4/tests/script.sh
+++ Lab4/tests/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-exe="../../../project/src/./frontend.exe"
+exe="../../../project/src/frontend.exe"
+transactions="../../../project/src/transactions.txt"
 accounts="../../../project/src/accounts.txt"
 tempCons="../../tempCons.txt"
+tempTrans="../../tempTrans.trans"
 
 cd inputs
 cd chng
@@ -14,7 +16,8 @@ do
     echo -e "\t\e[1;30;47m[ Running Test: $f ]\e[0m"
     # Pipe the input file into the program and then output the
     # console output to the tempCons file
-    cat $f | $exe $accounts > $tempCons 
+    cat $f | $exe $accounts > $tempCons
+
     # Check if there is a difference between the correct output
     # and the output of the test case
     checkCons=$(diff $tempCons ../../outputs/chng/${f%%.*}.out)
@@ -26,6 +29,20 @@ do
         # Output the test has failed in red
         echo -e "\t\e[1;31;41m[ Test Case Has Failed ]\e[0m"
     fi
-    # Output the test ended in white 
+
+    # Check if there is a difference between the transaction file
+    # and the output of the test case
+    checkTrans=$(diff $tempTrans $transactions)
+    if [ "$checkTrans" == "" ]
+    then
+        # Output the test has passed in green
+        echo -e "\t\e[1;32;42m[ (trans) Test Case Has Passed ]\e[0m"
+    else
+        # Output the test has failed in red
+        echo -e "\t\e[1;31;41m[ (trans) Test Case Has Failed ]\e[0m"
+    fi
+
+
+    # Output the test ended in white
     echo -e "\t\e[1;30;47m[ Finished Test: $f ]\e[0m\n"
 done
