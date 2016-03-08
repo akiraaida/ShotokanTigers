27cb2a6883c62bbb30025b7e3bf18116eb4f539b
akiraaida <akira.aida@live.com>
2016-03-07 21:09:53 -0500

    Further cleaned up script

--- project/tests/script.sh
+++ project/tests/script.sh
@@ -7,14 +7,14 @@ cd inputs
 for f in *.in;
 do
     # Create actual files for each test case (actual transfer and actual output)
-    tempCons=../${f%%.*}.aout
-    trans=../${f%%.*}.atf
-    touch $trans
-    # Pipe the input file into the program and then output the console output to the tempCons file
-    $exe $accounts $trans < $f > $tempCons
+    actOut=../${f%%.*}.aout
+    actTrans=../${f%%.*}.atf
+    touch $actTrans
+    # Pipe the input file into the program and then output the console output to the actOut file
+    $exe $accounts $actTrans < $f > $actOut
     # Check if there is a difference between the correct output and the output of the test case
-    checkCons=$(diff $tempCons ../outputs/${f%%.*}.out)
-    checkTrans=$(diff $trans ../outputs/${f%%.*}.trans)
+    checkCons=$(diff $actOut ../outputs/${f%%.*}.out)
+    checkTrans=$(diff $actTrans ../outputs/${f%%.*}.trans)
     if [ "$checkCons" == "" ] && [ "$checkTrans" == "" ]
     then
         # Output the test has passed in green
@@ -24,11 +24,11 @@ do
         echo -e "\e[1;30;41m[ '$f' Test Case Has Failed ]\e[0m"
         # Outputs the differences between the actual file and the output that should be there 
         echo -e "[ output ]"
-        diff $tempCons ../outputs/${f%%.*}.out -y
+        diff $actOut ../outputs/${f%%.*}.out -y
         echo -e "[ transactions ]"
-        diff $trans ../outputs/${f%%.*}.trans -y
+        diff $actTrans ../outputs/${f%%.*}.trans -y
     fi
-    # Move the temporary files to the actual files directory to compare actual/expected
-    mv $tempCons ../actual
-    mv $trans ../actual
+    # Move the temporary files to the actual files directory to compare actual/expected later
+    mv $actOut ../actual
+    mv $actTrans ../actual
 done
