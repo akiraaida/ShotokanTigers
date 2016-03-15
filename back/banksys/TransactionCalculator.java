79f4cd7a97441f390f1a53624e96a2d63faaf6fe
Kathryn McKay <kathryn.mckay@uoit.net>
2016-03-15 13:43:23 -0400

    coded most of each transaction method

--- back/banksys/TransactionCalculator.java
+++ back/banksys/TransactionCalculator.java
@@ -92,62 +92,142 @@ class TransactionCalculator {
     return null;
   }
   
-  private void handleWithdrawal(Vector<Transaction> transactions) {
+  private double getTransactionFee(Account account) {
     //todo
+    return 0.0;
+  }
+  
+  private boolean accountNumberExists(int number) {
+    for(Map.Entry<String, ArrayList<Account>> entry 
+        : account_table_.entrySet()) {
+      for(Account account : entry.getValue()) {
+        if(account.number == number) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  private void handleWithdrawal(Vector<Transaction> transactions) {
+    // Pop off stuff
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // Perform withdrawal
     Account account = getAccount(top);
-    double new_balance = account.balance - top.amount;
+    double account_fee = getTransactionFee(account);
+    double debit = top.amount + account_fee;
+    double new_balance = account.balance - debit;
     assert new_balance >= 0.0;
     account.balance = new_balance;
   }
 
   private void handleTransfer(Vector<Transaction> transactions) {
-    //todo
-    Transaction top = transactions.firstElement();
+    // Pop off first two
+    Transaction sender_transaction = transactions.firstElement();
     transactions.remove(0);
+    Transaction recipient_transaction = transactions.firstElement();
+    transactions.remove(0);
+    
+    // Check transaction
+    assert sender_transaction.amount == recipient_transaction.amount;
+    double amount = sender_transaction.amount;
+    
+    // Perform transfer
+    Account sender = getAccount(sender_transaction);
+    Account recipient = getAccount(recipient_transaction);
+    double sender_fee = getTransactionFee(sender);
+    double debit = sender_fee + amount;
+    double sender_balance = sender.balance - debit;
+    assert sender_balance >= 0.0;
+    sender.balance = sender_balance;
+    recipient.balance = recipient.balance + amount;
   }
 
   private void handlePaybill(Vector<Transaction> transactions) {
-    //todo
+    // Pop off first one
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // pay that bill
+    Account account = getAccount(top);
+    double account_fee = getTransactionFee(account);
+    double debit = top.amount + account_fee;
+    double new_balance = account.balance - debit;
+    assert new_balance >= 0.0;
+    account.balance = new_balance;
   }
 
   private void handleDeposit(Vector<Transaction> transactions) {
-    //todo
+    // Pop off the first one
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // Do a deposit
+    Account account = getAccount(top);
+    double account_fee = getTransactionFee(account);
+    double credit = top.amount - account_fee;
+    double new_balance = account.balance + credit;
+    assert new_balance >= 0.0;
+    account.balance = new_balance;
   }
 
   private void handleChangePlan(Vector<Transaction> transactions) {
-    //todo
+    // Pop off the transaction
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // Change the plan
+    assert top.misc.compareTo(" S") == 0 || top.misc.compareTo(" N") == 0;
+    Account account = getAccount(top);
+    account.is_student_plan = top.misc.compareTo(" S") == 0 ? true : false;
   }
 
   private void handleDelete(Vector<Transaction> transactions) {
-    //todo
+    // Pop off the transaction
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // Delete the dude
+    Account account = getAccount(top);
+    assert account != null;
+    account_table_.get(top.account_name).remove(account);
   }
 
   private void handleCreate(Vector<Transaction> transactions) {
-    //todo
+    // Pop off the transaction
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // ensure account number does not already exist
+    assert !accountNumberExists(top.account_number);
+    
+    // todo
   }
 
   private void handleDisable(Vector<Transaction> transactions) {
-    //todo
+    // Pop off the transaction
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // Disable the dude
+    Account account = getAccount(top);
+    assert account != null;
+    assert account.is_active == true;
+    account.is_active = false;
   }
 
   private void handleEnable(Vector<Transaction> transactions) {
-    //todo
+    // Pop off the transaction
     Transaction top = transactions.firstElement();
     transactions.remove(0);
+    
+    // Enable the dude
+    Account account = getAccount(top);
+    assert account != null;
+    assert account.is_active != true;
+    account.is_active = true;
   }
   
   /**
