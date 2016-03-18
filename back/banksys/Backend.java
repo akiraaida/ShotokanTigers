917e321bb4f418b527d5c45ce21df4d683856240
akiraaida <akira.aida@live.com>
2016-03-18 15:34:43 -0400

    Outputted the file updater to output a newCurr.txt

--- back/banksys/Backend.java
+++ back/banksys/Backend.java
@@ -1,61 +1,74 @@
+/**
+* backend.java
+* CSCI 3060U/SOFE 3980U: Course Project Back End
+* Winter 2016
+*
+* Shotokan Tigers:
+* -----
+* Akira Aida          100526064
+* Kathryn McKay       100524201
+* Alexander Wheadon   100514985
+*/
+
 package banksys;
 
 import java.util.*;
 
+
 public class Backend {
+    
+    /**
+    * The program will take a master accounts file and one or many transaction files
+    * and then create a new master accounts file with the transactions applied to it
+    * from the old master accounts file
+    * Input: master.txt trans1.txt trans2.txt trans3.txt
+    * Output: concat.txt newMaster.txt newCurr.txt
+    * Usage: make
+    * Usage: make run
+    */
     public static void main(String[] args){
         
-        // Execution requires at least two files to run. The master accounts file
-        // and one transaction file.
-        // Execution... java Backend master.txt trans1.txt trans2.txt
-        // Where there can be an unilimited number of transaction files
+        /**
+        * Execution requires at least two files to run. The master accounts file
+        * and one transaction file.
+        * Execution... java Backend master.txt trans1.txt trans2.txt
+        * Where there can be an unilimited number of transaction files
+        */
         if(args.length < 2){
             System.out.println("ERROR, NOT ENOUGH ARGUEMENTS.");
             System.exit(0);
         }
 
-        // Open filestream
+        /**
+        * Load all of the files in the master accounts file into the data
+        * structure map with the name as the key and their account information
+        * in the data structure "Account", "Account"s are in an ArrayList if they
+        * have multiple accounts
+        */
         FileParser parse = new FileParser();
-
-        // Load all of the files in the master accounts file into the data
-        // structure map with the name as the key and their account information
-        // in the data structure "Account". "Account"s are in an ArrayList if they
-        // have multiple accounts
         Map<String, ArrayList<Account>> account_table = parse.parseMaster(args[0]);
         
-        // Add all of the transaction files to an ArrayList
         List<String> transFiles = new ArrayList<String>();
         for(int i = 1; i < args.length; i++){
             transFiles.add(args[i]);
         }
         
-        // Concatenate all of the given transaction files and put all of their contents
-        // into "concat.txt"
+        /**
+        * Concatenate all of the given transaction files and put all of their contents
+        * into "concat.txt"
+        */
         parse.concatTrans(transFiles);
 
-        // Put all of the transactions that are in "concat.txt" into a vector
         Vector<Transaction> transactions = new Vector<Transaction>(parse.parseTrans());
         
-        // Apply transactions
+        // Apply the transactions to the map
         TransactionCalculator transaction_calculator
             = new TransactionCalculator();
         transaction_calculator.setAccountTable(account_table);
         transaction_calculator.applyTransactions(transactions);
 
-        // Printout file
+        // Write the updated accounts to file
         FileUpdater file_updater = new FileUpdater();
         file_updater.fileWriter(transaction_calculator.getAccountTable());
-
-
-        // TODO: Debit the accounts 0.05 for each student plan transaction or 0.10 for
-        // each non-student plan transaction
-
-
-
-        // TODO: Reformat to fit Google style
-
-
-
-        // TODO: Comment functions and classes
     }
 }
